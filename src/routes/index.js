52c0397621a25a8e53b8a1ca97440b30284860db
Douglas Araujo <doug.araujos@gmail.com>
2017-12-20 16:56:53 -0200

    [FEATURE][ADD] - Adicionar arquivo com configuração das rotas
    Adicionar arquivo com configuração das rotas.
    
    [AUTHOR] = Douglas Araujo <doug.araujos@gmail.com>
    [BRANCH] = feature/routes

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+/**
+ * Module Dependencies
+ */
+
+import Home from 'pages/Home';
+import Product from 'pages/Product';
+import Search from 'pages/Search';
+import Error404 from 'pages/Error404';
+
+
+const routes = [
+	{
+		path: '/',
+		component: Home,
+		exact: true
+	},
+	{
+		path: '/items/',
+		component: Search,
+		exact: true
+	},
+	{
+		path: '/items/:id',
+		component: Product,
+		exact: true
+	},
+	{
+		path: '*',
+		component: Error404
+	}
+];
+
+export default routes;
